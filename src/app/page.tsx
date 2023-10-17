ae1ee4955202d5e5c86108950fb820176933ef1f
Matthieu Tricoire <76185622+MatthieuTricoire@users.noreply.github.com>
2023-10-17 21:11:19 +0100

    feat: Prismic slice machine init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main>
+      <h1 className="text-8xl font-heading">Test</h1>
+      <p className=" font-body">Test</p>
+    </main>
   );
 }
