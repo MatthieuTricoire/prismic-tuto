2690be6a1608378bdc4c769b69442de8998ece81
Matthieu Tricoire <76185622+MatthieuTricoire@users.noreply.github.com>
2023-10-17 21:51:42 +0100

    feat : add fallback to metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-8xl font-heading">Test</h1>
-      <p className=" font-body">Test</p>
+      <p className=" font-body">Test !!!</p>
     </main>
   );
 }
